24cb3a2404caa642d1aaa6f1a480b2df24d4ef93
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2022-07-23 13:09:29 +0100

    TopSection done

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class Colors {
+  static Color backgroundColor = const Color(0xFFFCFEFF);
+  static Color accentColor = const Color(0xFF3380EB);
+  static Color grayColor = const Color(0xFFF1F1FA);
+  static Color recordColor = const Color(0xFFE59FA2);
+  static Color iconBackgroundColor = const Color(0xFFEDF5FD);
+  static Color secondaryTextColor = const Color(0xFFD0D0D2);
+}
